fb4580468587cd27827fc3582837de74cd4c3a52
NegasaReta <167341232+NegasaReta@users.noreply.github.com>
2025-02-06 10:14:52 +0300

    Update vanya_and_fence.py

--- vanya_and_fence.py
+++ vanya_and_fence.py
@@ -1,1 +1,11 @@
-//The code below here
+def main():
+    n, h = map(int, input().split())
+    a = list(map(int, input().split()))
+    width = 0
+    for i in a:
+        if i > h:
+            width += 2
+        else:
+            width += 1
+    print(width)
+main()
